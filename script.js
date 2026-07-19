1f7e0047d2106d263c3a998eab4891e621c2096a
Ditercio Silva <32583620+Ditercio@users.noreply.github.com>
2026-07-18 22:13:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('leadForm').addEventListener('submit', function(e) {
+    // Aqui seria a lógica de integração com Systeme.io/Eventos
+    console.log('Lead Capturado');
+});
